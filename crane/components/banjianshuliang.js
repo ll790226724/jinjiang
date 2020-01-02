94514f442d5423f629d20dbd0a722a06786ecd05
idcjaja <838927992@qq.com>
2020-01-02 17:18:15 +0800

    feat: add digital component

--- crane/components/banjianshuliang.js
+++ crane/components/banjianshuliang.js
@@ -1,4 +1,29 @@
 module.exports = {
   id: 'banlishuliang',
-  component: 'div'
+  component: 'div',
+  children: [
+    {
+      component: '@byzanteam/vis-components/digital-roll',
+      props: {
+        titlePosition: 'left',
+        $content: {
+          title: '当月办件数量',
+          $digital: 1876,
+          suffix: '件'
+        },
+        $digitalStyle: {
+          color: '#ffffff',
+          fontSize: '26px'
+        },
+        $titleStyle: {
+          color: '#ffffff',
+          fontSize: '14px'
+        },
+        $suffixStyle: {
+          color: '#ffffff',
+          fontSize: '14px'
+        }
+      }
+    }
+  ],
 }
