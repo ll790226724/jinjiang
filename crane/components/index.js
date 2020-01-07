6df75deeec9eb831c81b9bc5d9e40dc971c84896
idcjaja <838927992@qq.com>
2020-01-07 10:07:37 +0800

    fix: line circle size

--- crane/components/index.js
+++ crane/components/index.js
@@ -53,8 +53,8 @@ module.exports = {
         bb: { name: '不满意', type: 'bar', yAxisIndex: 0, stack: true, barWidth: 12 },
         cc: { name: '无法判断满意状况', type: 'bar', yAxisIndex: 0, stack: true, barWidth: 12 },
         dd: { name: '基本满意', type: 'bar', yAxisIndex: 0, stack: true, barWidth: 12 },
-        ee: { name: '回访情况', type: 'line', yAxisIndex: 1 },
-        ff: { name: '平均回复时间', type: 'line', yAxisIndex: 1 }
+        ee: { name: '回访情况', type: 'line', yAxisIndex: 1, symbolSize: 8 },
+        ff: { name: '平均回复时间', type: 'line', yAxisIndex: 1, symbolSize: 8 }
       }
     },
     {
