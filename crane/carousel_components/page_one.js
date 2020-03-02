ac6c499f45d72036e7abb2986cfaabbdef4e43d3
idcjaja <838927992@qq.com>
2020-03-02 16:33:27 +0800

    feat: add pageone bar chart

--- crane/carousel_components/page_one.js
+++ crane/carousel_components/page_one.js
@@ -91,6 +91,109 @@ module.exports = {
               }
             }
           ]
+        },
+        {
+          component: 'div',
+          position: [49, 477],
+          props: {
+            $style: {
+              height: '1px',
+              width: '1200px',
+              borderBottom: '1px solid #2e2e2e',
+              opacity: '.1'
+            }
+          }
+        },
+        {
+          component: 'div',
+          position: [42, 537],
+          props: {
+            $style: {
+              boxSizing: 'content-box',
+              height:'6px',
+              width: '6px',
+              borderRadius: '5px',
+              borderWidth: '2px',
+              borderColor: '#2E2E2E',
+              borderStyle: 'solid',
+            }
+          },
+        },
+        {
+          component: 'div',
+          position: [65, 528],
+          props: {
+            $style: {
+              color: '#2E2E2E',
+              fontSize: '18px',
+              fontWeight: '500',
+              textAlign: 'left',
+              letterSpacing: '0.9',
+            },
+          },
+          content: '诉求类型',
+        },
+        {
+          component: '@byzanteam/vis-components/data-loader',
+          position: [41, 584],
+          exports: {
+            results: 'results',
+          },
+          props: {
+            $url:"`/v1/components/f4b74ddd-39de-493f-84ab-9d87fcf23fee/data?start=${craneStates.filterRange[0]}&end=${craneStates.filterRange[1]}`",
+            method: 'get',
+            $data: "[{label: '投诉类型', amount: 12}]",
+            $style: {
+              width: '1200px',
+              height: '370px'
+            },
+          },
+          children: [
+            {
+              component: '@byzanteam/graphite/vertical-bar',
+              props: {
+                'v-if': 'results',
+                $data: "results.map((result) => ({label: result[1], '数量（件）': result[0]}))",
+                labelKey: 'label',
+                valueKey: '数量（件）',
+                $mainAxis: {
+                  $labelStyle: {
+                    $rotate: -45,
+                    $size: 14,
+                    fill: '#666666',
+                  },
+                  $labelLength: 7,
+                  $lineStyle: {
+                    stroke: 'transparent',
+                  },
+                },
+                $crossAxis: {
+                  $range: {
+                    $count: 5,
+                  },
+                  $lineStyle: {
+                    stroke: 'transparent',
+                  },
+                  $labelStyle: {
+                    $size: 16,
+                    fill: '#666666',
+                  },
+                  unit: {
+                    content: '件',
+                    fill: '#666666'
+                  },
+                },
+                $gap: {
+                  $outer: 3
+                },
+                $series: "['#1b74ef']",
+                $theme: {
+                  background: 'transparent',
+                },
+                ...chartTooltipOptions
+              }
+            }
+          ]
         }
       ]
     }
