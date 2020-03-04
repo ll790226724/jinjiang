f6997d32e145756cf96f2dbf392c512fd2c71ce1
renhaihuang <790226724@qq.com>
2020-03-04 10:59:59 +0800

    feat: all department in vis

--- crane/carousel_components/page_two.js
+++ crane/carousel_components/page_two.js
@@ -242,6 +242,59 @@ module.exports = {
       },
       content: '排名后三'
     },
+    {
+      id: 'department-ranking',
+      component: '@byzanteam/vis-components/data-loader',
+      position: [417, 307],
+      exports: {
+        results: 'results',
+      },
+      props: {
+        $url: "`/v1/components/c9b74ddd-39de-493f-84ab-9d87fcf23fee/data?start=${craneStates.filterRange[0]}&end=${craneStates.filterRange[1]}`",
+        method: 'get',
+        $data: "[{label: '承办单位', amount: 12}]",
+        $style: {
+          width: '436px',
+          maxHeight: '413px',
+          padding: '8px',
+          overflow: 'scroll'
+        },
+      },
+      children: [
+        {
+          id: 'department-ranking-content',
+          component: '@byzanteam/vis-components/ranking',
+          props: {
+            'v-if': '!craneStates.rank',
+            $data: "results.map(item => { return {label: item[1], amount: item[0] } } )",
+            $keys: {
+              label: 'label',
+              value: 'amount',
+              tooltip: 'name'
+            },
+            $labelStyle: {
+              color: '#666666',
+              fontSize: '16px',
+              lineHeight: '24px',
+              fontWeight: '400'
+            },
+            $valueStyle: {
+              color: '#2E2E2E',
+              fontSize: '16px',
+              lineHeight: '1.5',
+              fontWeight: 400,
+            },
+            $lineStyle: {
+              background: 'rgba(46, 46, 46, 0.05)',
+              $lineColor: "['#1B74EF', '#1B74EF80']",
+              height: '3px',
+              borderRadius: '3px'
+            },
+            ...chartTooltipOptions
+          },
+        },
+      ],
+    },
     // 事件来源
     {
       id: 'page-2-event-source-circle',
